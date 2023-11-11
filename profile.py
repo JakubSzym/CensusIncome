c660b786f445d68a613839d9f868d61c59850214
JakubSzym <kubaszym77@gmail.com>
2023-11-11 14:33:36 +0100

    Made data report using pandas profiling

---
+++ profile.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+df = pd.read_csv("adult-data.csv")
+profile = ProfileReport(df, title="Census Income Data Report")
+profile.to_file("data_report.html")
